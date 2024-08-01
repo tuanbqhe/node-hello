3b93bc651e7d8401ad8d5ef3ff643fa3d8a79287
quoctuan <tuanbqhe141256@gmail.com>
2024-08-02 01:54:46 +0700

    fix: build with platform linux/amd64

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3003;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Jenkin Aws docker-hub'
+  const msg = 'Hello Jenkin Aws DockerHub'
   res.end(msg);
 });
 

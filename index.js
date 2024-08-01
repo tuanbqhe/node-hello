6d6bde7378982fc0d26403fa7c5af0fd8225c073
quoctuan <tuanbqhe141256@gmail.com>
2024-08-01 14:44:19 +0700

    feat: build

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3003;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello Jenkin\n'
   res.end(msg);
 });
 
